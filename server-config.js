4cd84d6029703a824946d44169eda44b2a0cf2e5
HumbleToughAmazingMark <mark.andrews@encounter.sa.edu.au>
2026-09-06 09:24:37 +0930

    Use permanent Render websocket server

--- server-config.js
+++ server-config.js
@@ -1,2 +1,2 @@
 // Set this to the public WebSocket URL for server.js when deploying to GitHub Pages.
-window.PULSE_DUEL_SERVER_URL = 'wss://growing-recreation-seniors-click.trycloudflare.com';
+window.PULSE_DUEL_SERVER_URL = 'wss://gam-vtsl.onrender.com';
