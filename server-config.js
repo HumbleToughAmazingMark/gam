e896c98b83d9d2c0d9f844fafddf05f48648f4f5
HumbleToughAmazingMark <mark.andrews@encounter.sa.edu.au>
2026-09-05 21:59:59 +0930

    --

--- server-config.js
+++ server-config.js
@@ -1,2 +1,2 @@
 // Set this to the public WebSocket URL for server.js when deploying to GitHub Pages.
-window.PULSE_DUEL_SERVER_URL = 'https://gam-vtsl.onrender.com';
+window.PULSE_DUEL_SERVER_URL = 'wss://growing-recreation-seniors-click.trycloudflare.com';
